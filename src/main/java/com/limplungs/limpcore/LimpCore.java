6f43c63ddb30c8af0272c41416b1ac052950cb49
LimpLungs <xlaxlimplungs@gmail.com>
2016-08-08 16:33:25 -0500

    initial commit for LimpCore 2.0dev1

---
+++ src/main/java/com/limplungs/limpcore/LimpCore.java
@@ -0,0 +1,18 @@
+package com.limplungs.limpcore;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = LimpCore.MODID, version = LimpCore.VERSION)
+public class LimpCore
+{
+    public static final String MODID = "limpcore";
+    public static final String VERSION = "2.0dev1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
